eaf7f53898262e9be51855edded1502bf1014cfd
jsc <youme@youmedeiMac.local>
2019-06-14 16:02:56 +0800

    no message

---
+++ learnOpenGL/learnOpenGL/BumperCars/car/base/SceneModel.h
@@ -0,0 +1,36 @@
+//
+//  SceneModel.h
+//  learnOpenGL
+//
+//  Created by you&me on 2019/6/14.
+//  Copyright © 2019 you&me. All rights reserved.
+//
+
+#import <GLKit/GLKit.h>
+
+@class AGLKVertexAttribArrayBuffer;
+@class SceneMesh;
+NS_ASSUME_NONNULL_BEGIN
+
+
+// 边界，注意min和max 都是vector3
+typedef struct {
+    GLKVector3 min;
+    GLKVector3 max;
+}
+SceneAxisAllignedBoundingBox;
+@interface SceneModel : NSObject
+@property(nonatomic,copy)NSString *name;
+@property(nonatomic,assign)SceneAxisAllignedBoundingBox axisAlignedBoundingBox;
+
+-(id)initWithName:(NSString *)aName
+             mesh:(SceneMesh *)aMesh
+ numberOfVertices:(GLsizei)aCount;
+
+-(void)draw;
+
+-(void)updateAlignedBoundingBOxForVertices:(float *)verts
+                                     count:(unsigned int)aCount;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ learnOpenGL/learnOpenGL/BumperCars/car/base/SceneModel.m
@@ -0,0 +1,13 @@
+//
+//  SceneModel.m
+//  learnOpenGL
+//
+//  Created by you&me on 2019/6/14.
+//  Copyright © 2019 you&me. All rights reserved.
+//
+
+#import "SceneModel.h"
+
+@implementation SceneModel
+
+@end
